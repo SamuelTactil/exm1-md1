b097627c15ca5c983e0a5785d04bcc3c624bab63
adriancho39 <adriancho10@yandex.com>
2025-10-03 10:06:11 +0200

    no me sale cuadrarlo

---
+++ script-adrian.js
@@ -0,0 +1,15 @@
+
+function sumarPares(n) {
+    let suma = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 === 0) {
+            suma += i;
+        }
+    }
+    return suma;
+}
+
+function tablaMultiplicar(n) {
+    for (let i = n; i <= tope; tope++) {
+
+    }
